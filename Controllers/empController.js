174ec7d723a3bc3c594c059d52ae6524c9cb0d0c
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-07-20 17:10:13 +0530

    Added ADD API'S

--- Controllers/empController.js
+++ Controllers/empController.js
@@ -1,10 +1,5 @@
 const empService = require("../Services/empService");
 
-// GET ALL
-const getAllEmployees = (req, res) => {
-    res.status(200).json(empService.getAllEmployees());
-};
-
 // POST
 const addEmployee = (req, res) => {
     const employee = empService.addEmployee(req.body);
@@ -15,72 +10,6 @@ const addEmployee = (req, res) => {
     });
 };
 
-// GET BY ID
-const getEmployeeById = (req, res) => {
-    const employee = empService.getEmployeeById(req.params.id);
-
-    if (!employee) {
-        return res.status(404).json({
-            message: "Employee Not Found"
-        });
-    }
-
-    res.status(200).json(employee);
-};
-
-// PUT
-const updateEmployee = (req, res) => {
-    const employee = empService.updateEmployee(req.params.id, req.body);
-
-    if (!employee) {
-        return res.status(404).json({
-            message: "Employee Not Found"
-        });
-    }
-
-    res.status(200).json({
-        message: "Employee Updated Successfully",
-        data: employee
-    });
-};
-
-// PATCH
-const patchEmployee = (req, res) => {
-    const employee = empService.patchEmployee(req.params.id, req.body);
-
-    if (!employee) {
-        return res.status(404).json({
-            message: "Employee Not Found"
-        });
-    }
-
-    res.status(200).json({
-        message: "Employee Patched Successfully",
-        data: employee
-    });
-};
-
-// DELETE
-const deleteEmployee = (req, res) => {
-    const employee = empService.deleteEmployee(req.params.id);
-
-    if (!employee) {
-        return res.status(404).json({
-            message: "Employee Not Found"
-        });
-    }
-
-    res.status(200).json({
-        message: "Employee Deleted Successfully",
-        data: employee
-    });
-};
-
 module.exports = {
-    getAllEmployees,
-    addEmployee,
-    getEmployeeById,
-    updateEmployee,
-    patchEmployee,
-    deleteEmployee
+    addEmployee
 };
--- Services/empService.js
+++ Services/empService.js
@@ -1,9 +1,6 @@
 const employees = require("../emp");
 
-// GET ALL
-const getAllEmployees = () => {
-    return employees;
-};
+
 
 // POST
 const addEmployee = (employee) => {
@@ -11,57 +8,7 @@ const addEmployee = (employee) => {
     return employee;
 };
 
-// GET BY ID
-const getEmployeeById = (id) => {
-    return employees.find(emp => emp.id === Number(id));
-};
 
-// PUT
-const updateEmployee = (id, updatedEmployee) => {
-
-    const index = employees.findIndex(emp => emp.id === Number(id));
-
-    if (index === -1) {
-        return null;
-    }
-
-    employees[index] = updatedEmployee;
-
-    return employees[index];
-};
-
-// PATCH
-const patchEmployee = (id, data) => {
-
-    const employee = employees.find(emp => emp.id === Number(id));
-
-    if (!employee) {
-        return null;
-    }
-
-    Object.assign(employee, data);
-
-    return employee;
-};
-
-// DELETE
-const deleteEmployee = (id) => {
-
-    const index = employees.findIndex(emp => emp.id === Number(id));
-
-    if (index === -1) {
-        return null;
-    }
-
-    const deletedEmployee = employees.splice(index, 1)[0];
-
-    return deletedEmployee;
-};
 module.exports = {
-    getAllEmployees,
-    addEmployee,
-    getEmployeeById,
-    updateEmployee,
-    patchEmployee,
-    deleteEmployee
+    addEmployee
 };
