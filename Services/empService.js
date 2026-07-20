93b2dd6074cd446241db028a6a286cdafccde6ee
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-07-20 17:14:57 +0530

    Added UPDATE API'S

--- Services/empService.js
+++ Services/empService.js
@@ -1,20 +1,5 @@
 const employees = require("../emp");
 
-// GET ALL
-const getAllEmployees = () => {
-    return employees;
-};
-
-// POST
-const addEmployee = (employee) => {
-    employees.push(employee);
-    return employee;
-};
-
-// GET BY ID
-const getEmployeeById = (id) => {
-    return employees.find(emp => emp.id === Number(id));
-};
 
 // PUT
 const updateEmployee = (id, updatedEmployee) => {
@@ -44,24 +29,8 @@ const patchEmployee = (id, data) => {
     return employee;
 };
 
-// DELETE
-const deleteEmployee = (id) => {
-
-    const index = employees.findIndex(emp => emp.id === Number(id));
-
-    if (index === -1) {
-        return null;
-    }
-
-    const deletedEmployee = employees.splice(index, 1)[0];
-
-    return deletedEmployee;
-};
 module.exports = {
-    getAllEmployees,
-    addEmployee,
-    getEmployeeById,
+
     updateEmployee,
-    patchEmployee,
-    deleteEmployee
+    patchEmployee
 };
