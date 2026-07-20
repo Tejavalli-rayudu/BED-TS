0a07afbe5fae28515b52cad0b940d0228c4afa73
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-07-20 17:18:06 +0530

    Added DELETE API'S

--- Services/empService.js
+++ Services/empService.js
@@ -1,49 +1,5 @@
 const employees = require("../emp");
 
-// GET ALL
-const getAllEmployees = () => {
-    return employees;
-};
-
-// POST
-const addEmployee = (employee) => {
-    employees.push(employee);
-    return employee;
-};
-
-// GET BY ID
-const getEmployeeById = (id) => {
-    return employees.find(emp => emp.id === Number(id));
-};
-
-// PUT
-const updateEmployee = (id, updatedEmployee) => {
-
-    const index = employees.findIndex(emp => emp.id === Number(id));
-
-    if (index === -1) {
-        return null;
-    }
-
-    employees[index] = updatedEmployee;
-
-    return employees[index];
-};
-
-// PATCH
-const patchEmployee = (id, data) => {
-
-    const employee = employees.find(emp => emp.id === Number(id));
-
-    if (!employee) {
-        return null;
-    }
-
-    Object.assign(employee, data);
-
-    return employee;
-};
-
 // DELETE
 const deleteEmployee = (id) => {
 
@@ -58,10 +14,6 @@ const deleteEmployee = (id) => {
     return deletedEmployee;
 };
 module.exports = {
-    getAllEmployees,
-    addEmployee,
-    getEmployeeById,
-    updateEmployee,
-    patchEmployee,
+
     deleteEmployee
 };
