8b85d637acb2202a49eb7c4bfd5793295ab0b4eb
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-07-20 16:42:21 +0530

    Added Emp-get API'S

--- Routes/empRoutes.js
+++ Routes/empRoutes.js
@@ -4,10 +4,5 @@ const router = express.Router();
 const empController = require("../Controllers/empController");
 
 router.get("/", empController.getAllEmployees);
-router.post("/", empController.addEmployee);
 router.get("/:id", empController.getEmployeeById);
-router.put("/:id", empController.updateEmployee);
-router.patch("/:id", empController.patchEmployee);
-router.delete("/:id", empController.deleteEmployee);
 
-module.exports = router;
