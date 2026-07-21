bbc4962e7221b983e56a5b72d9ef9198fcd3ec84
Tejavalli-rayudu <tejavallirayudu@gmail.com>
2026-07-21 16:25:12 +0530

    Added auth

--- Routes/studentRoutes.js
+++ Routes/studentRoutes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const studentController = require("../Controllers/studentController");
-const authMiddleware =require("../middleware/authMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
+
+// Protect all routes below
+router.use(authMiddleware);
 
 router.get("/", studentController.getAllStudents);
 router.post("/", studentController.addStudent);
 router.get("/:id", studentController.getStudentById);
-router.get("/qurey", studentController.getStudentsByQuery);
+router.get("/query", studentController.getStudentsByQuery);
 router.put("/:id", studentController.updateStudent);
 router.patch("/:id", studentController.patchStudent);
 router.delete("/:id", studentController.deleteStudent);
